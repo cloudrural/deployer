206c8e10565e520853769ed4a40069a9fc7c477b
bhavya <bhavya633@gmail.com>
2020-06-10 21:11:28 +0530

    made the required changes

--- ec2-instance/my-instance.tf
+++ ec2-instance/my-instance.tf
@@ -1,13 +1,10 @@
 provider "aws" {
-access_key = "${var.aws_access_key}"
-secret_key = "${var.aws_secret_key}"
-region = "${var.aws_region}"
+access_key = "AKIAI7TJT6RNYYZP5UNA"
+secret_key = "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"
+region = "us-east-2"
 }
 
 resource "aws_instance" "web_server" {
 ami = "ami-0d03add87774b12c5"
 instance_type = "t2.micro"
-tags {
-Name = "my_server"
-}
 }
